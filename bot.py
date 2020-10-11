0f0bdcd3a34d93ac5aadc66ec7cee27e274294c8
Sarthak Agrawal <sarthavader@gmail.com>
2020-10-11 14:20:46 -0500

    added weekly

--- bot.py
+++ bot.py
@@ -46,10 +46,10 @@ async def daily(ctx):
 
         await ctx.send("This server has sent " + str(count) + " messages today.")
 
+
 @bot.command()
 async def date(ctx, year, month, day):
-
-    ctx.send("WARNING! THIS TAKES A VERY LONG TIME. Please be patient:)")
+    ctx.send("WARNING! THIS MAY TAKE A VERY LONG TIME. Please be patient:)")
     async with ctx.channel.typing():
         after = datetime.datetime(int(year), int(month), int(day))
         before = datetime.datetime(int(year), int(month), int(day) + 1)
@@ -61,6 +61,22 @@ async def date(ctx, year, month, day):
             await ctx.send(str(channel) + " has " + str(len(messages)) + " messages.")
 
         await ctx.send("This server has sent " + str(count) + " messages on " + str(after))
+
+
+async def week(ctx):
+    days = [0,1,2,3,4,5,6]
+    async with ctx.channel.typing():
+        for day in days:
+            date = datetime.date.today()
+            after = datetime.datetime(date.year, date.month, date.day - day)
+            before = datetime.datetime(date.year, date.month, date.day - day + 1)
+            count = 0
+            for channel in ctx.guild.text_channels:
+                messages = await channel.history(after=after, before=before, limit=None).flatten()
+                count += len(messages)
+
+            await ctx.send("This server has sent " + str(count) + " messages on " + str(after))
+
 
 
 bot.run(TOKEN)
