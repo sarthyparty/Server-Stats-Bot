cf1a931aea726ccbf8fb6762e39eeaa5c11ec4e5
Sarthak Agrawal <sarthavader@gmail.com>
2020-10-11 13:57:06 -0500

    added typing

--- bot.py
+++ bot.py
@@ -34,30 +34,31 @@ async def hey(ctx):
 
 @bot.command()
 async def daily(ctx):
-    my_date = datetime.date.today()
-    today = datetime.datetime(my_date.year, my_date.month, my_date.day)
-    count = 0
-    for channel in ctx.guild.text_channels:
-        await ctx.send("Counting messages in " + str(channel))
-        messages = await channel.history(after=today, limit = None).flatten()
-        count += len(messages)
-        await ctx.send(str(channel) + " has " + str(len(messages)) + " messages.")
+    async with ctx.channel.typing():
+        my_date = datetime.date.today()
+        today = datetime.datetime(my_date.year, my_date.month, my_date.day)
+        count = 0
+        for channel in ctx.guild.text_channels:
+            await ctx.send("Counting messages in " + str(channel))
+            messages = await channel.history(after=today, limit=None).flatten()
+            count += len(messages)
+            await ctx.send(str(channel) + " has " + str(len(messages)) + " messages.")
 
-    await ctx.send("This server has sent " + str(count) + " messages today.")
-
+        await ctx.send("This server has sent " + str(count) + " messages today.")
 
 @bot.command()
 async def date(ctx, year, month, day):
-    after = datetime.datetime(int(year), int(month), int(day))
-    before = datetime.datetime(int(year), int(month), int(day) + 1)
-    count = 0
-    for channel in ctx.guild.text_channels:
-        await ctx.send("Counting messages in " + str(channel))
-        messages = await channel.history(after=after, before=before, limit=None).flatten()
-        count += len(messages)
-        await ctx.send(str(channel) + " has " + str(len(messages)) + " messages.")
+    async with ctx.channel.typing():
+        after = datetime.datetime(int(year), int(month), int(day))
+        before = datetime.datetime(int(year), int(month), int(day) + 1)
+        count = 0
+        for channel in ctx.guild.text_channels:
+            await ctx.send("Counting messages in " + str(channel))
+            messages = await channel.history(after=after, before=before, limit=None).flatten()
+            count += len(messages)
+            await ctx.send(str(channel) + " has " + str(len(messages)) + " messages.")
 
-    await ctx.send("This server has sent " + str(count) + " messages on " + str(after))
+        await ctx.send("This server has sent " + str(count) + " messages on " + str(after))
 
 
 bot.run(TOKEN)
